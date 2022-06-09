5adbe18853c4350793e7e070a6f9836add4b8388
akila-niasa <akilaniasa@gmail.com>
2022-06-09 15:16:26 +0600

    update navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,51 +1,26 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './NavBar.css'
 
 const NavBar = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" className='navbar' variant="dark">
   <Container>
   <Navbar.Brand href="#home">PortFolio</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-    <Link className='text-light m-2'  to="/">Home</Link>
+    <Nav className="me-auto nav-item">
+    <Link className='text-light m-2 nav-link'  to="/">Home</Link>
 
-    <Link className='text-light m-2' to="/contact">Contact</Link>
-      {/* <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-     
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link> */}
+    <Link className='text-light m-2 nav-link' to="/contact">Contact</Link>
+      
 
     </Nav>
   </Navbar.Collapse>
   </Container>
 </Navbar>
-       
-    //     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    //    <div class="container">
-    //     {/* <Link to='/home' class="navbar-brand"><img src={logo} alt=""/></Link> */}
-    //     <button class="navbar-toggler navbar-toggat" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    //       <span class="navbar-toggler-icon hamburger-icon"></span>
-    //     </button>
-    //     <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-    //        <ul class="navbar-nav mb-2 mb-lg-0 mx-auto pt-4">
-    //        <li class="nav-item">
-    //                 <Link  to="/">Home</Link>
-    //             </li>
-    //        <li class="nav-item">
-    //                 <Link  to="/contact">Contact</Link>
-    //             </li>
-    //        </ul>
-    //        </div>
-    //     </div>
-    //   </nav>
+  
     );
 };
 
