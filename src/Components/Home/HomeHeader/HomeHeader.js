8ddd46b335af7c664f555042076f0c829b4d557a
akila-niasa <akilaniasa@gmail.com>
2022-06-09 09:34:12 +0600

    first commit

---
+++ src/Components/Home/HomeHeader/HomeHeader.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './HomeHeader.css'
+import { FaGithubAlt,FaLinkedinIn,FaTwitter,FaCloudDownloadAlt } from 'react-icons/fa';
+
+const HomeHeader = () => {
+    return (
+        <main
+        className=" main-div row d-flex align-items-center"
+    // style={{ height: "350px" }}
+    >
+        <div className="social-part d-flex align-items-center pt-5">
+            <ul>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/akila-niasa">
+                        <FaGithubAlt/>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/athqiya-akila-30718522a/">
+                        <FaLinkedinIn/>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://twitter.com/athqiya_akila">
+                        <FaTwitter/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div className="col-md-12 text-center pt-5">
+          
+            <h3 className="name-heading pt-3">ATHQIYA AKILA</h3>
+            <h1 className="banner-heading pb-2">{''}
+
+           
+               Fornt-end developer
+            </h1>
+            
+           <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1mYS8uhDWBgLy6C3C7mfgrvnnz5ff7JmF/view">
+               <FaCloudDownloadAlt/>
+               Download</a>
+           
+        </div>
+    </main>
+    );
+};
+
+export default HomeHeader;
